c42457a38c00a2a26bbcc3ba744134ba82dc35b3
Bruno Carvalho <bruno.carvalho769@gmail.com>
2022-05-02 20:09:07 +0100

    Added titles to graphs

--- tools/graph.py
+++ tools/graph.py
@@ -19,42 +19,35 @@ def label_to_color(label):
 def main():
     parser = argparse.ArgumentParser(description="Generate plot from CSV data.")
     parser.add_argument("dataset", nargs="*", help="Path to CSV file.")
-    parser.add_argument("--reference", help="Path to CSV file.")
-    parser.add_argument("--compare", nargs="*", help="Path to CSV file.")
     parser.add_argument("--threed", action='store_true', help="Make a 3d scatterplot.")
     args = parser.parse_args()
     Path("graphs").mkdir(exist_ok=True)
-
-    if args.reference is not None and args.compare is not None:
-        normal = pd.read_csv(args.reference, usecols=[0, 1], names=["Entropy", "Hamming distance"])
-        for path in args.compare:
-            compare = pd.read_csv(path, usecols=[0, 1, 2], names=["Entropy", "Hamming distance", "Label"])
-            ax = plt.figure().add_subplot()
-            ax.scatter(normal["Entropy"], normal["Hamming distance"], label=args.reference)
-            ax.scatter(compare["Entropy"], compare["Hamming distance"], c=compare["Label"].apply(label_to_color).values if "Label" in compare else "c", label=path)
-            plt.xlabel("Entropy")
-            plt.ylabel("Hamming distance")
-            plt.legend(loc="best")
-            plt.savefig(f"graphs/{Path(path).parent.name}.png")
-    else:
-        for path in args.dataset:
-            Path(f"graphs/{Path(path).stem}").mkdir(exist_ok=True)
-            dataset = pd.read_csv(path)
-            if args.threed:
-                fig = plt.figure()
-                ax = fig.add_subplot(projection='3d')
-                ax.set_xlabel("AvgTime")
-                ax.set_ylabel("Entropy")
-                ax.set_zlabel("HammingDist")
-                ax.scatter(dataset["AvgTime"], dataset["Entropy"], dataset["HammingDist"], c=dataset["Label"].apply(label_to_color).values if "Label" in dataset else "c")
-                plt.savefig(f"graphs/{Path(path).stem}.png")
-            for col in dataset:
-                if col != "Label":
-                    figure(figsize=(15, 5))
-                    plt.xlabel("Window")
+    for path in args.dataset:
+        Path(f"graphs/{Path(path).stem}").mkdir(exist_ok=True)
+        dataset = pd.read_csv(path)
+        if args.threed:
+            fig = plt.figure()
+            ax = fig.add_subplot(projection='3d')
+            ax.set_xlabel("AvgTime")
+            ax.set_ylabel("Entropy")
+            ax.set_zlabel("HammingDist")
+            ax.scatter(dataset["AvgTime"], dataset["Entropy"], dataset["HammingDist"], c=dataset["Label"].apply(label_to_color).values if "Label" in dataset else "c")
+            plt.savefig(f"graphs/{Path(path).stem}.png")
+        for col in dataset:
+            if col != "Label":
+                figure(figsize=(15, 5))
+                plt.xlabel("Window")
+                plt.scatter(np.arange(0, len(dataset[col])), dataset[col], c=dataset["Label"].apply(label_to_color).values if "Label" in dataset else "c")
+                if col == "AvgTime":
+                    plt.ylabel("Average Time")
+                    plt.title("Average Time Between Packets of the Same ID")
+                elif col == "Entropy":
                     plt.ylabel(col)
-                    plt.scatter(np.arange(0, len(dataset[col])), dataset[col], c=dataset["Label"].apply(label_to_color).values if "Label" in dataset else "c")
-                    plt.savefig(f"graphs/{Path(path).stem}/{col}.png")
+                    plt.title("Average Entropy of Packets of the Same ID")
+                elif col == "HammingDist":
+                    plt.ylabel("Hamming Distance")
+                    plt.title("Average Hamming Distance of Packets of the Same ID")
+                plt.savefig(f"graphs/{Path(path).stem}/{col}.png")
 
 
 if __name__ == "__main__":
